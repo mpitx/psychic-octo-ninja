cb575f7be725e2ae7f304859e9e8b9f1db85d8dd
kennyballou <kballou@onyx.boisestate.edu>
2013-07-22 17:09:44 -0600

    Add inner time runs

--- time_vector_sum_cudampi.sh
+++ time_vector_sum_cudampi.sh
@@ -2,5 +2,6 @@
 
 for i in $(seq 0 3)
 do
-    /usr/bin/time python2 vector_sum_cudampi.py 2> ~/Data/out/vsum_cudampi_${i}
+    /usr/bin/time mpi_run.py /usr/bin/time python2 \
+     vector_sum_cudampi.py 2> ~/Data/out/vsum_cudampi_${i}
 done
--- time_vector_sum_mpi.sh
+++ time_vector_sum_mpi.sh
@@ -2,5 +2,6 @@
 
 for i in $(seq 0 3)
 do
-    /usr/bin/time python2 vector_sum_mpi.py 2> ~/Data/out/vsum_mpi_${i}
+    /usr/bin/time mpi_run.py /usr/bin/time python2 \
+     vector_sum_mpi.py 2> ~/Data/out/vsum_mpi_${i}
 done
